85b290284745407cdf8c1f9c92bc434b4771a85f
Jonas96Lundin <62259168+Jonas96Lundin@users.noreply.github.com>
2022-02-23 14:24:54 +0100

    Added nuget psql

---
+++ Gamification/Assets/Scripts/DatabaseTest.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Npgsql;
+
+public class DatabaseTest : MonoBehaviour
+{
+    private NpgsqlConnection conn;
+    int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectToDatabase("aj8015");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NpgsqlCommand cmd;
+            cmd = new NpgsqlCommand("INSERT INTO test (personid, namn) values(:personid, :namn)", conn);
+			cmd.Parameters.Add(new NpgsqlParameter("personid", counter));
+			cmd.Parameters.Add(new NpgsqlParameter("namn", "TestNamn" + counter++));
+			cmd.ExecuteNonQuery();
+		}
+    }
+
+    private void ConnectToDatabase(string dataBase)
+    {
+        conn = new NpgsqlConnection("Server=pgserver.mah.se; User Id=aj8015; Password=fbninmt0; Database=" + dataBase);
+        conn.Open();
+    }
+}
