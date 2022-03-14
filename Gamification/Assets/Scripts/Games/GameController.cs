5b3e53d5ef14c0230040ba7bca29571821c74a62
Jonas96Lundin <62259168+Jonas96Lundin@users.noreply.github.com>
2022-03-14 19:37:08 +0100

    Made repository static class

--- Gamification/Assets/Scripts/Games/GameController.cs
+++ Gamification/Assets/Scripts/Games/GameController.cs
@@ -10,17 +10,20 @@ public class GameController : MonoBehaviour
 	public void OnContinue()
 	{
 		//Save points (pointController.CurrentPoints)
+		Repository.AddToColorwheelLeaderboard(pointController.CurrentPoints);
 		SceneManager.LoadScene(1);
 	}
 
 	public void OnReset()
 	{
 		//Save points (pointController.CurrentPoints) to database
+		Repository.AddToColorwheelLeaderboard(pointController.CurrentPoints);
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 
 	public void OnQuitGame()
 	{
+		Repository.GetColorWheelLeaderboard();
 		SceneManager.LoadScene(1);
 	}
 }
